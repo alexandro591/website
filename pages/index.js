587230e759eba0773e07060c9e886a576f2d72b0
alexandro591 <alexandrotapiaflores@gmail.com>
2021-02-28 23:29:51 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,29 @@ export default function App() {
       <Head>
         <title>Alexandro</title>
         <link rel="icon" href="/logo.png" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.fbAsyncInit = function () {
+                FB.init({
+                  xfbml: true,
+                  version: "v10.0",
+                });
+              };
+            
+              (function (d, s, id) {
+                var js,
+                  fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+                fjs.parentNode.insertBefore(js, fjs);
+              })(document, "script", "facebook-jssdk");
+            `,
+          }}
+        />
+        <script dangerouslySetInnerHTML={{ __html: `` }} />
       </Head>
       <NavBar></NavBar>
       {(() => {
@@ -39,6 +62,12 @@ export default function App() {
           return <About></About>;
         }
       })()}
+      <div id="fb-root"></div>
+      <div
+        className="fb-customerchat"
+        attribution="setup_tool"
+        page_id="101039808207496"
+      ></div>
     </div>
   );
 }
