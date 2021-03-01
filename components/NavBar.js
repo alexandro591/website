2a6f2b512d9d70d6b45d9d9a76377cb1d04f607c
alexandro591 <alexandrotapiaflores@gmail.com>
2021-02-28 20:29:13 -0500

    added song

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,5 @@
 import styles from "../styles/Global.module.css";
+import { useEffect, useState } from "react";
 import {
   Box,
   Image,
@@ -10,11 +11,37 @@ import {
 } from "../shared/chakra";
 export default function NavBar(props) {
   const { src } = props;
+  const [playerStatus, setPlayerStatus] = useState(false);
+  const [audio, setAudio] = useState(null);
+  useEffect(() => {
+    if (audio) {
+      audio.oncanplaythrough = function () {
+        audio.play();
+        setPlayerStatus(true);
+      };
+    }
+  }, [audio]);
   return (
     <>
       <Box className={styles.navbar}>
         <Box w="100px">
-          <Image src="/logo.png" />
+          <Image
+            src="/logo.png"
+            className={playerStatus ? "playerOn" : ""}
+            onClick={(e) => {
+              if (!audio) setAudio(new Audio("/song.mp3"));
+              else {
+                if (playerStatus) {
+                  audio.pause();
+                  setPlayerStatus(false);
+                } else {
+                  audio.currentTime = 0;
+                  audio.play();
+                  setPlayerStatus(true);
+                }
+              }
+            }}
+          />
         </Box>
         <Box className={styles.navbar_options}>
           <Box>
